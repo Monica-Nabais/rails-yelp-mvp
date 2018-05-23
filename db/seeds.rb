0112c305583950bed01ec001f775e05dd0672b0d
Monica Nabais <monicamnabais@gmail.com>
2018-05-23 15:24:18 +0100

    rails yelp mvp exercise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Restaurant.destroy_all
+
+10.times do
+  Restaurant.create(name: Faker::Dessert.topping, address: Faker::Address.street_address, phone_number: Faker::Number.number(9).to_s, category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+end
+
+50.times do
+  Review.create(content: Faker::HarryPotter.quote, rating: rand(0..5), restaurant_id: rand(0..9))
+end
